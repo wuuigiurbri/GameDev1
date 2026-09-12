af61935aae73ccce85d4bb2bcd9b64736d34053c
wuuigiurbri <120124489+wuuigiurbri@users.noreply.github.com>
2026-09-12 13:58:13 -0400

    Add files via upload

---
+++ Game Dev 1/Lesson 4_ Tuples/lesson.py
@@ -0,0 +1,11 @@
+list3 = ["happy", 41, 32.5, 8237, "ivoi"]
+print(type(list3))
+tuple1= ("happy", 41, 32.5, 8237, "ivoi")
+print(type(tuple1))
+
+tuple2= "happy", 41 , 32.5, 8237, "ivoi"
+print(type(tuple2))
+tuple3= tuple1, tuple2
+print(tuple3)
+tuple4= ("haiej", ["happy", 41, 32.5, 8237, "ivoi"], ("idoje","ndoj",928,283))
+print(tuple1[0])
