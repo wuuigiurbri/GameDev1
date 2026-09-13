196c0e128940f8d1b690484dcff83e304268656e
wuuigiurbri <120124489+wuuigiurbri@users.noreply.github.com>
2026-09-13 15:52:41 -0400

    Add files via upload

--- Game Dev 1/Lesson 4_ Tuples/lesson.py
+++ Game Dev 1/Lesson 4_ Tuples/lesson.py
@@ -8,4 +8,18 @@ print(type(tuple2))
 tuple3= tuple1, tuple2
 print(tuple3)
 tuple4= ("haiej", ["happy", 41, 32.5, 8237, "ivoi"], ("idoje","ndoj",928,283))
-print(tuple1[0])
+print(len(tuple4))
+print(tuple4[2][2])
+print(tuple1 [0:3])
+print(tuple1[:])
+print(tuple2[2:])
+print(tuple4[::-1])
+my_tuple= (4,2,3, [6,5])
+list3 [0] = "sad"
+print(list3)
+my_tuple [3][0] = 16
+print(my_tuple)
+petuble= ("Teniola",14,"Garth Street","Westdale")
+name, age, street, school= petuble
+print(name, age, street, school)
+print(petuble)
